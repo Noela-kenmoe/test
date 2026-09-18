e205d876cfb6116d6d936422c5e22a520e559ad9
Noela-kenmoe <kenmoenoela@gmail.com>
2026-09-18 23:27:10 +0200

    modifier le fichier fon.cpp cette modification va servire pour la fusion des deux branche

---
+++ fon.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+ int echange(int *a, int *b);
+
+int main(){
+    
+    int y, c ,d;
+    std::cout<<"entrer deux nombres :";
+    std::cin>> c >> d;
+    std::cout<<"avant l'appel:"<< c << d;
+    y =echange(&c,&d);
+     std::cout<<"apres l'appel :"<< c << y ;
+     std::cout<<"verification de l'appel";
+     return 0;
+     
+}
+int echange (int *a, int *b)
+     { 
+      
+        int x;
+        *a = x;
+        *a = *b;
+        *b = x;
+        return 0;
+     }
